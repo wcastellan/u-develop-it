afb2e2f17d19bf17f906964560a3fe5e265c3ba3
Wyatt C <wcastellan@yahoo.com>
2021-12-20 10:52:16 -0500

    add, delete, single out a candidate

--- server.js
+++ server.js
@@ -25,13 +25,37 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
+//db.query(`SELECT * FROM candidates`, (err, row) => {
+    //console.log(rows);
+//});
 
+// GET a single candidate
+//db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    //if (err) {
+        //console.log(err);
+    //}
+    //console.log(row);
+//});
 
+// Delete a candidate
+//db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    //if (err) {
+        //console.log(err);
+    //}
+    //console.log(result);
+//});
 
+// Create a candidate
+//const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                //VALUES (?,?,?,?)`;
+//const params = [1, 'Ronald', 'FirBank', 1];
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
+//db.query(sql, params, (err, result) => {
+    //if (err) {
+        //console.log(err);
+    //}
+    //console.log(result);
+//});
 
 // default response for amny other request (Not Found)
 app.use((req, res) => {
