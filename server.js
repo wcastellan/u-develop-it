afcb8a6d789d51ea71bcb0190de4c88c5dcb57fe
Wyatt C <wcastellan@yahoo.com>
2021-12-20 10:30:23 -0500

    add database to server

--- server.js
+++ server.js
@@ -5,23 +5,36 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// connect to MySQL database
+const mysql = require('mysql2');
+
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
-});
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your MySQL username
+        user: 'root',
+        // your MySQL password
+        password: 'L@p1z123',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+
+
+
+
+
 
 // default response for amny other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
-
-
-
 
 // start express.js server on port 3001
 app.listen(PORT, () => {
